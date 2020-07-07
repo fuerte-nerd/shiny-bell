6183cfb556d02874ea742a38e3ca9a42d816bc4f
David Andrews <dandrewsuk82@gmail.com>
2020-07-07 18:10:54 +0100

    Autogit: 07-07-2020 18:10

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -32,6 +32,38 @@ const Buttons = () => {
           </Button>
         </Grid>
       </Grid>
+      <Grid container justify="center" spacing={1} xs={12}>
+        <Grid item>
+          <Button size="large" variant="outlined">
+            Default
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button size="large" variant="outlined" color="primary">
+            Primary
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button size="large" variant="outlined" color="secondary">
+            Secondary
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button size="large" variant="contained">
+            Default
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button size="large" variant="contained" color="primary">
+            Primary
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button size="large" variant="contained" color="secondary">
+            Secondary
+          </Button>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
