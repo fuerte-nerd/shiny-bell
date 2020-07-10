ca9b1f9e3bfea5ca8164f4789ff17b5939673fcb
David Andrews <dandrewsuk82@gmail.com>
2020-07-10 14:38:13 +0100

    Autogit: 10-07-2020 14:38

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -21,7 +21,12 @@ const Sidebar = ({ dispatch, settings }) => {
   };
 
   return (
-    <Drawer anchor="right" open={settings} onClose={handleClose}>
+    <Drawer
+      anchor="right"
+      open={settings}
+      onClose={handleClose}
+      BackdropProps={{ style: { backgroundColor: "transparent" } }}
+    >
       <List dense disablePadding style={{ width: 250, fontFamily: "Roboto" }}>
         <Locks />
         <Fonts />
