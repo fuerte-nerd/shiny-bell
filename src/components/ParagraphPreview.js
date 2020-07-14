e48e095a98de4c418699849d629d1896b2b51dce
David Andrews <dandrewsuk82@gmail.com>
2020-07-14 18:15:57 +0100

    Autogit: 14-07-2020 18:15

--- src/components/ParagraphPreview.js
+++ src/components/ParagraphPreview.js
@@ -40,9 +40,7 @@ const ParagraphPreview = ({
         bgcolor={backgrounds.box}
         p={backgrounds.box !== "none" ? 2 : 0}
       >
-        <Typography variant="h1">
-          Welcome to your new Material-UI theme!
-        </Typography>
+        <Typography variant="h1">Welcome to your new theme!</Typography>
         <Typography paragraph>
           The {twoFonts ? "body font" : "font"} is {bodyFont.themeName}
           {twoFonts && ` and the header font is ${headerFont.themeName}`}. The
