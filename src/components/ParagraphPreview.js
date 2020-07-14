158331533e4b1a761c05b1739e4f530a5f1b127c
David Andrews <dandrewsuk82@gmail.com>
2020-07-14 12:31:57 +0100

    Autogit: 14-07-2020 12:31

--- src/components/ParagraphPreview.js
+++ src/components/ParagraphPreview.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Typography, Box } from "@material-ui/core";
+import { Divider, Typography, Box } from "@material-ui/core";
 
 const ParagraphPreview = ({ bodyFont, headerFont, twoFonts }) => {
   return (
-    <Box>
-      <Typography variant="h1">
-        Welcome to your new Material-UI theme!
-      </Typography>
-      <Typography paragraph>
-        The {twoFonts ? "body font" : "font"} is {bodyFont.themeName}
-        {twoFonts && ` and the header font is ${headerFont.themeName}`}.
-      </Typography>
-    </Box>
+    <>
+      <Box my={4}>
+        <Typography variant="h1">
+          Welcome to your new Material-UI theme!
+        </Typography>
+        <Typography paragraph>
+          The {twoFonts ? "body font" : "font"} is {bodyFont.themeName}
+          {twoFonts && ` and the header font is ${headerFont.themeName}`}.
+        </Typography>
+      </Box>
+      <Divider />
+    </>
   );
 };
 
