cfa337f628f4598aeffe2066e31bb4508d74bd49
David Andrews <dandrewsuk82@gmail.com>
2020-07-14 16:54:57 +0100

    Autogit: 14-07-2020 16:54

--- src/functions/codeExporter.js
+++ src/functions/codeExporter.js
@@ -16,7 +16,7 @@ export default () => {
 
   code += ` } from "@material-ui/core"\n\n`;
 
-  // body
+  // palette
   code += `const theme = createMuiTheme({\n${tab}palette: {\n`;
 
   if (state.mode === "dark") {
@@ -26,7 +26,48 @@ export default () => {
   code += `${tab + tab}primary: { main: "${state.primary}" },\n`;
   code += `${tab + tab}secondary: { main: "${state.secondary}" }\n`;
 
-  code += `${tab}}\n})\n\n`;
+  code += `${tab}},\n`;
+  // typography
+
+  code += `${tab}typography: {\n`;
+
+  if (state.twoFonts) {
+    code += `${tab + tab}h1: { fontFamily: "${
+      state.headerFont.themeName
+    }" },\n`;
+    code += `${tab + tab}h2: { fontFamily: "${
+      state.headerFont.themeName
+    }" },\n`;
+    code += `${tab + tab}h3: { fontFamily: "${
+      state.headerFont.themeName
+    }" },\n`;
+    code += `${tab + tab}h4: { fontFamily: "${
+      state.headerFont.themeName
+    }" },\n`;
+    code += `${tab + tab}h5: { fontFamily: "${
+      state.headerFont.themeName
+    }" },\n`;
+    code += `${tab + tab}h6: { fontFamily: "${
+      state.headerFont.themeName
+    }" },\n`;
+    code += `${tab + tab}subtitle1: { fontFamily: "${
+      state.font.themeName
+    }" },\n`;
+    code += `${tab + tab}subtitle2: { fontFamily: "${
+      state.font.themeName
+    }" },\n`;
+    code += `${tab + tab}body1: { fontFamily: "${state.font.themeName}" },\n`;
+    code += `${tab + tab}body2: { fontFamily: "${state.font.themeName}" },\n`;
+    code += `${tab + tab}button: { fontFamily: "${state.font.themeName}" },\n`;
+    code += `${tab + tab}overline: { fontFamily: "${
+      state.font.themeName
+    }" },\n`;
+    code += `${tab + tab}caption: { fontFamily: "${state.font.themeName}" },\n`;
+  }
+
+  code += `${tab}},\n`;
+
+  code += `})\n\n`;
 
   code += `export default `;
 
