144591be2a90606a0c9824aeec15a5a0fcd2bd28
David Andrews <dandrewsuk82@gmail.com>
2020-07-16 15:26:40 +0100

    Autogit: 16-07-2020 15:26

---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const Layout = () => {
+  return null;
+};
+
+const mapStateToProps = (state) => ({});
+
+export default connect(mapStateToProps)(Layout);
