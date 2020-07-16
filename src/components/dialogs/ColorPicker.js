d8a679921cfb0a2e500fa096d67fedd06f5da7da
David Andrews <dandrewsuk82@gmail.com>
2020-07-16 17:02:19 +0100

    Autogit: 16-07-2020 17:02

---
+++ src/components/dialogs/ColorPicker.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+import { setPrimary, setColorPicker } from "../../state/actions";
+import { Dialog } from "@material-ui/core";
+import { SketchPicker } from "react-color";
+
+const ColorPicker = ({ dispatch, colorPicker, primary }) => {
+  return (
+    <Dialog
+      open={colorPicker}
+      onClose={() => dispatch(setColorPicker(false))}
+      PaperProps={{ style: { backgroundColor: "transparent" } }}
+      BackdropProps={{ style: { backgroundColor: "transparent" } }}
+    >
+      <SketchPicker
+        color={primary}
+        onChange={(c) => dispatch(setPrimary(c.hex))}
+      />
+    </Dialog>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  colorPicker: state.colorPicker,
+  primary: state.primary,
+});
+
+export default connect(mapStateToProps)(ColorPicker);
