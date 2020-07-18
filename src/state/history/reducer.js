9487d63490be582ff82f20a8c231432e51edf7a9
David Andrews <dandrewsuk82@gmail.com>
2020-07-18 18:27:43 +0100

    Autogit: 18-07-2020 18:27

--- src/state/history/reducer.js
+++ src/state/history/reducer.js
@@ -1,3 +1,5 @@
+import { SET_PAST, SET_CURRENT } from "./types";
+
 const initialState = {
   past: [],
   current: null,
@@ -7,4 +9,22 @@ const initialState = {
     validated: false,
     changes: null,
   },
+};
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+
+  const newState = Object.assign({}, state);
+
+  switch (type) {
+    case SET_PAST:
+      newState.past = payload;
+      break;
+    case SET_CURRENT:
+      newState.current = payload;
+    default:
+      break;
+  }
+
+  return newState;
 };
