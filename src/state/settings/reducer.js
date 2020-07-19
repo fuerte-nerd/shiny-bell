9ffa95361dabaf4c181374c4d834a77fce20b5ff
David Andrews <dandrewsuk82@gmail.com>
2020-07-19 12:17:41 +0100

    Autogit: 19-07-2020 12:17

---
+++ src/state/settings/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  randomFontSelect: true,
+  fontSize: 14,
+  responsiveFontSizes: true,
+  twoFonts: false,
+  darkMode: false,
+  secondaryColorMix: "complement",
+  rounding: 4,
+  spacing: 8,
+  buttonTextTransform: "uppercase",
+  backgrounds: {
+    page: "transparent",
+    box: "transparent",
+  },
+};
