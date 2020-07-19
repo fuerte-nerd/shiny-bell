c5d9c1ebab3a34f8c457552dfa99fda3eb7f6d06
David Andrews <dandrewsuk82@gmail.com>
2020-07-19 12:12:36 +0100

    Autogit: 19-07-2020 12:12

--- src/state/viewables/reducer.js
+++ src/state/viewables/reducer.js
@@ -1,6 +1,38 @@
+import {
+  SET_LOADING_SCREEN,
+  SET_SIDEBAR,
+  SET_FONT_SELECTOR,
+  SET_COLOR_PICKER,
+} from "./types";
+
 const initialState = {
   loadingScreen: true,
   sidebar: false,
   fontSelector: false,
   colorPicker: false,
+};
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+
+  const newState = Object.assign({}, state);
+
+  switch (type) {
+    case SET_LOADING_SCREEN:
+      newState.loadingScreen = payload;
+      break;
+    case SET_SIDEBAR:
+      newState.sidebar = payload;
+      break;
+    case SET_FONT_SELECTOR:
+      newState.fontSelector = payload;
+      break;
+    case SET_COLOR_PICKER:
+      newState.colorPicker = payload;
+      break;
+    default:
+      break;
+  }
+
+  return newState;
 };
