e2dbe795930ec39158856084025a1828bd7842a2
David Andrews <dandrewsuk82@gmail.com>
2020-07-20 12:52:42 +0100

    Autogit: 20-07-2020 12:52

--- src/components/Head.js
+++ src/components/Head.js
@@ -9,43 +9,22 @@ const Head = ({ fonts, twoFonts, font, headerFont, fontValidation }) => {
 
   return (
     <Helmet>
-      {fontValidation.enabled
-        ? fontValidation.fonts.map((f) => (
-            <link
-              href={`https://fonts.googleapis.com/css2?family=${f.font.linkName}&display=swap`}
-              rel="stylesheet"
-            />
-          ))
-        : null}
-      {fonts && (
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+        rel="stylesheet"
+      />
+      {currentBodyFont && (
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          href={`https://fonts.googleapis.com/css2?family=${currentBodyFont.linkName}&display=swap`}
           rel="stylesheet"
         />
-      )}
-      {twoFonts && font && headerFont ? (
-        <link
-          href={`https://fonts.googleapis.com/css2?family=${font.linkName}&family=${headerFont.linkName}&display=swap`}
-          rel="stylesheet"
-        />
-      ) : (
-        font && (
-          <link
-            href={`https://fonts.googleapis.com/css2?family=${font.linkName}&display=swap`}
-            rel="stylesheet"
-          />
-        )
       )}
     </Helmet>
   );
 };
 
 const mapStateToProps = (state) => ({
-  fonts: state.fonts,
-  twoFonts: state.twoFonts,
-  font: state.font,
-  headerFont: state.headerFont,
-  fontValidation: state.fontValidation,
+  currentBodyFont: state.components.fonts.body.currentFont,
 });
 
 export default connect(mapStateToProps)(Head);
