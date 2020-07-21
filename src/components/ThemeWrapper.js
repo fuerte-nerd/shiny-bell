1777d7a819d986bd2810683ea67f308b27632ed7
David Andrews <dandrewsuk82@gmail.com>
2020-07-21 15:42:24 +0100

    Autogit: 21-07-2020 15:42

--- src/components/ThemeWrapper.js
+++ src/components/ThemeWrapper.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core";
+import {
+  ThemeProvider,
+  createMuiTheme,
+  responsiveFontSizes,
+} from "@material-ui/core";
 
 const ThemeWrapper = (props) => {
   const { children } = props;
@@ -9,67 +13,64 @@ const ThemeWrapper = (props) => {
 
   const { primaryColor, secondaryColor } = props;
 
+  const [theme, setTheme] = useState(
+    createMuiTheme({
+      overrides: {
+        MuiListItemText: {
+          primary: { fontFamily: "Roboto" },
+          secondary: { fontFamily: "Roboto" },
+        },
+      },
+      typography: {
+        h1: {
+          fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
+        },
+        h2: {
+          fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
+        },
+        h3: {
+          fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
+        },
+        h4: {
+          fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
+        },
+        h5: {
+          fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
+        },
+        h6: {
+          fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
+        },
+        subtitle1: {
+          fontFamily: bodyFont.themeName,
+        },
+        subtitle2: {
+          fontFamily: bodyFont.themeName,
+        },
+        body1: {
+          fontFamily: bodyFont.themeName,
+        },
+        body2: {
+          fontFamily: bodyFont.themeName,
+        },
+        button: {
+          fontFamily: bodyFont.themeName,
+        },
+        overline: {
+          fontFamily: bodyFont.themeName,
+        },
+        caption: {
+          fontFamily: bodyFont.themeName,
+        },
+      },
+      palette: {
+        primary: { main: primaryColor },
+        secondary: { main: secondaryColor },
+      },
+    })
+  );
+
   return (
-    bodyFont &&
-    headerFont && (
-      <ThemeProvider
-        theme={createMuiTheme({
-          overrides: {
-            MuiListItemText: {
-              primary: { fontFamily: "Roboto" },
-              secondary: { fontFamily: "Roboto" },
-            },
-          },
-          typography: {
-            h1: {
-              fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
-            },
-            h2: {
-              fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
-            },
-            h3: {
-              fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
-            },
-            h4: {
-              fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
-            },
-            h5: {
-              fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
-            },
-            h6: {
-              fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
-            },
-            subtitle1: {
-              fontFamily: bodyFont.themeName,
-            },
-            subtitle2: {
-              fontFamily: bodyFont.themeName,
-            },
-            body1: {
-              fontFamily: bodyFont.themeName,
-            },
-            body2: {
-              fontFamily: bodyFont.themeName,
-            },
-            button: {
-              fontFamily: bodyFont.themeName,
-            },
-            overline: {
-              fontFamily: bodyFont.themeName,
-            },
-            caption: {
-              fontFamily: bodyFont.themeName,
-            },
-          },
-          palette: {
-            primary: { main: primaryColor },
-            secondary: { main: secondaryColor },
-          },
-        })}
-      >
-        {children}
-      </ThemeProvider>
-    )
+    bodyFont && headerFont && <ThemeProvider theme={}>{children}</ThemeProvider>
   );
 };
 
