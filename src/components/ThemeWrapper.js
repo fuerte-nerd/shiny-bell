9524aaf42e066a6c05012728ffd97e6b8852fe41
David Andrews <dandrewsuk82@gmail.com>
2020-07-21 11:44:42 +0100

    Autogit: 21-07-2020 11:44

---
+++ src/components/ThemeWrapper.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { connect } from "react-redux";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
+
+const ThemeWrapper = (props) => {
+  const { children } = props;
+
+  const { twoFonts, bodyFont, headerFont } = props;
+
+  return (
+    <ThemeProvider
+      theme={createMuiTheme({
+        typography: {
+          h1: {
+            fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
+          },
+          h2: {
+            fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
+          },
+          h3: {
+            fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
+          },
+          h4: {
+            fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
+          },
+          h5: {
+            fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
+          },
+          h6: {
+            fontFamily: twoFonts ? headerFont.themeName : bodyFont.themeName,
+          },
+          subtitle1: {
+            fontFamily: bodyFont.themeName,
+          },
+          subtitle2: {
+            fontFamily: bodyFont.themeName,
+          },
+          body1: {
+            fontFamily: bodyFont.themeName,
+          },
+          body2: {
+            fontFamily: bodyFont.themeName,
+          },
+          button: {
+            fontFamily: bodyFont.themeName,
+          },
+          overline: {
+            fontFamily: bodyFont.themeName,
+          },
+          caption: {
+            fontFamily: bodyFont.themeName,
+          },
+        },
+      })}
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  twoFonts: state.settings.twoFonts,
+  bodyFont: state.components.fonts.body.currentFont,
+  headerFont: state.components.fonts.header.currentFont,
+});
+
+export default connect(mapStateToProps)(ThemeWrapper);
