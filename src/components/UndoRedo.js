c6a32d3b692788c1b9dc21cdbd6c07de044e3de9
David Andrews <dandrewsuk82@gmail.com>
2020-07-22 18:12:10 +0100

    Autogit: 22-07-2020 18:12

--- src/components/UndoRedo.js
+++ src/components/UndoRedo.js
@@ -16,6 +16,7 @@ const UndoRedo = (props) => {
   const { past, future, undoEnabled, componentsLoading } = props;
   const { body, header, primary, secondary } = props;
 
+  /*
   useEffect(() => {
     if (componentsLoading) {
       if (undoEnabled) {
@@ -35,7 +36,7 @@ const UndoRedo = (props) => {
       }
     }
   }, [componentsLoading]);
-
+  */
   const handleClick = (e) => {
     const { id } = e.currentTarget;
     switch (id) {
