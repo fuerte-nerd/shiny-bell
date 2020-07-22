0d7767a2b00221c37b2b984234ce36721def9812
David Andrews <dandrewsuk82@gmail.com>
2020-07-22 15:21:04 +0100

    Autogit: 22-07-2020 15:21

--- src/components/UndoRedo.js
+++ src/components/UndoRedo.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { IconButton, Tooltip } from "@material-ui/core";
-import { Undo, Redo } from "@material-ui/icons";
+import { Undo, Redo, Palette } from "@material-ui/icons";
+import { setComponentsLoading } from "../state/components/actions";
+import FontLoader from "../functions/FontHelper";
+import Palette from "../functions/Palette";
 
-const UndoRedo = () => {
+const UndoRedo = (props) => {
+  const { dispatch } = props;
+  const { past, current, future } = props;
+
+  const handleClick = (e) => {
+    const { id } = e.currentTarget;
+    switch (id) {
+      case "undo":
+        // grab the previous change
+        const previousAppState = past[past.length - 1];
+        //bypass setting an appstate
+        //commit it
+        dispatch(setComponentsLoading(true));
+        const bf = new FontLoader("body", previousAppState.body);
+        const hf = new FontLoader("header", previousAppState.header);
+        const palette = new Palette({
+          primary: previousAppState.primary.hex,
+          secondary: previousAppState.secondary.hex,
+        });
+    }
+  };
+
   return (
     <>
       <Tooltip title="Undo">
@@ -24,6 +48,10 @@ const UndoRedo = () => {
   );
 };
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  current: state.appState.current,
+  past: state.appState.past,
+  future: state.appState.future,
+});
 
 export default connect(mapStateToProps)(UndoRedo);
