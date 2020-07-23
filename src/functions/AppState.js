94f4de02e560349918390ffc530872f5d81d1a61
David Andrews <dandrewsuk82@gmail.com>
2020-07-23 16:05:16 +0100

    Autogit: 23-07-2020 16:05

--- src/functions/AppState.js
+++ src/functions/AppState.js
@@ -1,23 +1,57 @@
 import store from "../state/store";
+import Font from "./Font";
+import { setComponentsLoading } from "../state/components/actions";
 
 class AppState {
   constructor(config) {
-    this.fontLib = store.getState().library.fonts;
-    this.categories = {
-      body: store.getState().settings.fontCategoryFilters.body,
-      header: store.getState().settings.fontCategoryFilters.header,
-    };
-    if (config.fonts.body) {
-      this.body = config.fonts.body;
+    const state = store.getState();
+    if (!state.components.fonts.body.locked) {
+      if (config.body) {
+        this.body = new Font("body", config.body);
+      } else {
+        this.body = new Font("body");
+      }
     } else {
-      this.body = this.fetchRandomFont();
+      this.font = state.appState.current.body;
+    }
+    if (!state.components.fonts.header.locked) {
+      if (config.header) {
+        this.header = new Font("header", config.header);
+      } else {
+        this.header = new Font("header");
+      }
+    } else {
+      this.font = state.appState.current.header;
+    }
+
+    if (!state.components.palette.locked) {
+      if (config.primary) {
+        this.primary = config.palette.primary;
+      } else {
+        this.primary = this.getRandomColor();
+      }
     }
   }
 
-  fetchRandomFont() {
+  getRandomColor() {
+    const generateRandomNumber = () => {
+      return Math.floor(Math.random() * 255);
+    };
+
+    const rgb = `rgb(${generateRandomNumber()}, ${generateRandomNumber()}, ${generateRandomNumber()})`;
+
+    return tinycolor(rgb).toHexString();
+  }
+  init() {
+    store.dispatch(setComponentsLoading(true));
+  }
+
+  fetchRandomFont(target) {
     const fontSearchList = this.fontLib.filter((i) => {
-      return this.categories.includes(i.category);
+      return this.categories[target].includes(i.category);
     });
     return fontSearchList[Math.floor(Math.random() * fontSearchList.length)];
   }
 }
+
+export default AppState;
