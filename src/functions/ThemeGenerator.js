a291b763cbe3d165b855162b10906e4a79fdc1a4
David Andrews <dandrewsuk82@gmail.com>
2020-07-24 16:41:31 +0100

    Autogit: 24-07-2020 16:41

--- src/functions/ThemeGenerator.js
+++ src/functions/ThemeGenerator.js
@@ -6,26 +6,27 @@ import { setComponentsLoading } from "../state/components/actions";
 class Theme {
   constructor(config) {
     const state = store.getState();
+    const { appState, components } = state;
 
-    this.primary = state.components.palette.locked
-      ? state.appState.current.primary
+    this.primary = components.palette.locked
+      ? appState.current.primary
       : this.getRandomColor();
-    this.secondary = state.components.palette.locked
-      ? state.appState.current.secondary
+    this.secondary = components.palette.locked
+      ? appState.current.secondary
       : this.getSecondaryColor();
-    this.body = state.components.fonts.body.locked
-      ? state.appState.current.body
+    this.body = components.fonts.body.locked
+      ? appState.current.body
       : this.fetchRandomFont("body");
-    this.header = state.components.fonts.header.locked
-      ? state.appState.current.header
+    this.header = components.fonts.header.locked
+      ? appState.current.header
       : this.fetchRandomFont("header");
-    this.twoFonts = state.settings.twoFonts;
-    this.fontSize = state.settings.fontSize;
-    this.responsiveFontSizes = state.settings.responsiveFontSizes;
-    this.mode = state.settings.mode;
-    this.rounding = state.settings.rounding;
-    this.spacing = state.settings.spacing;
-    this.buttonTextTransform = state.settings.buttonTextTransform;
+    this.twoFonts = appState.current.twoFonts;
+    this.fontSize = appState.current.fontSize;
+    this.responsiveFontSizes = appState.current.responsiveFontSizes;
+    this.mode = appState.current.mode;
+    this.rounding = appState.current.rounding;
+    this.spacing = appState.current.spacing;
+    this.buttonTextTransform = appState.current.buttonTextTransform;
 
     this.bypassUndo = false;
     // overwrites
@@ -33,6 +34,10 @@ class Theme {
     for (const [key, value] of params) {
       this[key] = value;
     }
+  }
+
+  getProp(key) {
+    return this[key];
   }
 
   getRandomColor() {
@@ -50,15 +55,15 @@ class Theme {
 
     switch (mixMode) {
       case "complement":
-        return tinycolor(this.primary).complement().toHexString();
+        return tinycolor(this.getProp("primary")).complement().toHexString();
       case "desaturate":
-        return tinycolor(this.primary).desaturate(50).toHexString();
+        return tinycolor(this.getProp("primary")).desaturate(50).toHexString();
       case "saturate":
-        return tinycolor(this.primary).saturate(50).toHexString();
+        return tinycolor(this.getProp("primary")).saturate(50).toHexString();
       case "darken":
-        return tinycolor(this.primary).darken().toHexString();
+        return tinycolor(this.getProp("primary")).darken().toHexString();
       case "lighten":
-        return tinycolor(this.primary).lighten().toHexString();
+        return tinycolor(this.getProp("primary")).lighten().toHexString();
       default:
         return;
     }
