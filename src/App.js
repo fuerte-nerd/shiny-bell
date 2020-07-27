1eeb861fa5f9c0db8a23b940ce0b51e121bce2f2
David Andrews <dandrewsuk82@gmail.com>
2020-07-27 14:28:35 +0100

    Autogit: 27-07-2020 14:28

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
 import { Container, Toolbar, Box } from "@material-ui/core";
