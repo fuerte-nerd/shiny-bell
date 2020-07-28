6e52f23c2bd068730618e10daad92b7206509853
David Andrews <dandrewsuk82@gmail.com>
2020-07-28 17:07:21 +0100

    Autogit: 28-07-2020 17:07

--- src/components/GoogleFontValidator.js
+++ src/components/GoogleFontValidator.js
@@ -7,6 +7,10 @@ const GoogleFontValidator = (props) => {
 
   return (
     <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+        rel="stylesheet"
+      />
       {validationFont && (
         <link
           rel="stylesheet"
