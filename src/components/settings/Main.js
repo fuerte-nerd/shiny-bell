6cc48fc68858592ec9f0b0c43f8c067f11c297e6
David Andrews <dandrewsuk82@gmail.com>
2020-07-29 16:58:02 +0100

    Autogit: 29-07-2020 16:58

--- src/components/settings/Main.js
+++ src/components/settings/Main.js
@@ -1,19 +1,40 @@
 import React from "react";
-import { ListItem, ListItemText, ListItemIcon } from "@material-ui/core";
+import { connect } from "react-redux";
+import {
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+} from "@material-ui/core";
 import { Code } from "@material-ui/icons";
 import Setting from "../Setting";
+import { setThemeCode } from "../../state/display/actions";
 
-const Main = () => {
+const Main = ({ dispatch }) => {
+  const handleClick = (e) => {
+    const { id } = e.currentTarget;
+    switch (id) {
+      case "code":
+      case "code-btn":
+        dispatch(setThemeCode(true));
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <Setting title="Utility">
       <ListItem id="code" onClick={handleClick} button>
         <ListItemText primary="Get the code!" />
-        <ListItemIcon>
-          <Code />
-        </ListItemIcon>
+        <ListItemSecondaryAction>
+          <IconButton edge="end" id="code-btn" onClick={handleClick}>
+            <Code />
+          </IconButton>
+        </ListItemSecondaryAction>
       </ListItem>
     </Setting>
   );
 };
 
-export default Main;
+export default connect()(Main);
