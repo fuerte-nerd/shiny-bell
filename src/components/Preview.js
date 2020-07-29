08764cd72c3e4a7626d10a73c17d127b4c216208
David Andrews <dandrewsuk82@gmail.com>
2020-07-29 19:54:14 +0100

    Autogit: 29-07-2020 19:54

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -28,7 +28,12 @@ const Preview = ({ twoFonts, primary, secondary, current }) => {
         p={current.backgrounds.box !== "transparent" ? 4 : 0}
         bgcolor={current.backgrounds.box}
       >
-        <>
+        <Box
+          minHeight="100vh"
+          width="100vw"
+          display="flex"
+          justifyContent="center"
+        >
           <Typography variant="h1">Welcome to your new theme!</Typography>
           <Typography variant="subtitle1" paragraph>
             The {twoFonts ? `header font ` : `font `} is{" "}
@@ -38,19 +43,19 @@ const Preview = ({ twoFonts, primary, secondary, current }) => {
             . The primary color is {colorNames.primary} and the secondary color
             is {colorNames.secondary}.
           </Typography>
-          <Typography paragraph>
-            Consectetur officia assumenda magni cupiditate perspiciatis
-            voluptatem consequuntur? Eos veniam consequatur rem earum corrupti
-            Omnis veniam iste laboriosam neque quibusdam, dolorum Eum possimus
-            maiores magnam doloribus suscipit. Autem numquam numquam delectus
-            magni quam et Temporibus provident optio vel labore tempora?
-            Excepturi vel quis perferendis eos non. Distinctio reiciendis
-            facilis debitis qui cum Aperiam ipsum nesciunt facere debitis nisi
-            eos Officia doloribus ipsa ea eos quo distinctio ipsum Dolore ex
-            nisi quod eos ratione natus blanditiis Voluptatem reiciendis quam
-            qui expedita tempora ipsam Officia harum quia.
-          </Typography>
-        </>
+        </Box>
+        <Typography paragraph>
+          Consectetur officia assumenda magni cupiditate perspiciatis voluptatem
+          consequuntur? Eos veniam consequatur rem earum corrupti Omnis veniam
+          iste laboriosam neque quibusdam, dolorum Eum possimus maiores magnam
+          doloribus suscipit. Autem numquam numquam delectus magni quam et
+          Temporibus provident optio vel labore tempora? Excepturi vel quis
+          perferendis eos non. Distinctio reiciendis facilis debitis qui cum
+          Aperiam ipsum nesciunt facere debitis nisi eos Officia doloribus ipsa
+          ea eos quo distinctio ipsum Dolore ex nisi quod eos ratione natus
+          blanditiis Voluptatem reiciendis quam qui expedita tempora ipsam
+          Officia harum quia.
+        </Typography>
       </Box>
       <Text />
       <Buttons />
