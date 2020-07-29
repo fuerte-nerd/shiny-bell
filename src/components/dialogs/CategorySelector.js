af7bbe9bf7ddbdfeda1574c21f4af9e99b7297ac
David Andrews <dandrewsuk82@gmail.com>
2020-07-29 13:01:32 +0100

    Autogit: 29-07-2020 13:01

--- src/components/dialogs/CategorySelector.js
+++ src/components/dialogs/CategorySelector.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import {
   Dialog,
   DialogTitle,
+  Box,
+  FormGroup,
   FormControlLabel,
   Checkbox,
   DialogContent,
@@ -14,7 +16,69 @@ const Categories = ({ dispatch, twoFonts, isOpen, section }) => {
   return (
     <Dialog open={isOpen} maxWidth="lg">
       <DialogTitle>Set {twoFonts ? section : null} font filters</DialogTitle>
-      <DialogContent></DialogContent>
+      <DialogContent>
+        <Box mb={2}>
+          <FormGroup row>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  id="serif"
+                  size="small"
+                  onChange={handleChange}
+                  checked={isOpen && filters.includes("serif")}
+                />
+              }
+              label="Serif"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  id="sans-serif"
+                  size="small"
+                  onChange={handleChange}
+                  checked={filters.includes("sans-serif")}
+                />
+              }
+              label="Sans Serif"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  id="display"
+                  size="small"
+                  onChange={handleChange}
+                  checked={filters.includes("display")}
+                />
+              }
+              label="Display"
+            />
+          </FormGroup>
+          <FormGroup row>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  id="handwriting"
+                  size="small"
+                  onChange={handleChange}
+                  checked={filters.includes("handwriting")}
+                />
+              }
+              label="Handwriting"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  id="monospace"
+                  size="small"
+                  onChange={handleChange}
+                  checked={filters.includes("monospace")}
+                />
+              }
+              label="Monospace"
+            />
+          </FormGroup>
+        </Box>
+      </DialogContent>
       <DialogActions>
         <Button></Button>
         <Button></Button>
