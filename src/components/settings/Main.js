d7bafad760680a689f637064c480f8b829befefa
David Andrews <dandrewsuk82@gmail.com>
2020-07-29 17:08:19 +0100

    Autogit: 29-07-2020 17:08

--- src/components/settings/Main.js
+++ src/components/settings/Main.js
@@ -6,14 +6,42 @@ import {
   ListItemSecondaryAction,
   IconButton,
 } from "@material-ui/core";
-import { Code } from "@material-ui/icons";
+import { Undo, Redo, Code } from "@material-ui/icons";
 import Setting from "../Setting";
-import { setThemeCode } from "../../state/display/actions";
+import { setThemeCode, setLoadingScreen } from "../../state/display/actions";
+import {
+  setPastAppStates,
+  setFutureAppStates,
+} from "../../state/appState/actions";
+import Theme from "../Theme";
 
-const Main = ({ dispatch, future }) => {
+const Main = ({ dispatch, past, current, future }) => {
   const handleClick = (e) => {
+    let theme;
     const { id } = e.currentTarget;
     switch (id) {
+      case "undo":
+      case "undo-btn":
+        dispatch(setLoadingScreen(true));
+        dispatch(setFutureAppStates([current, ...future]));
+        theme = new Theme(past[past.length - 1]);
+        theme.validateFonts().then(() => {
+          theme.commit().then(() => dispatch(setLoadingScreen(false)));
+        });
+
+        dispatch(setPastAppStates(past.slice(0, past.length - 1)));
+
+        break;
+      case "redo":
+      case "redo-btn":
+        dispatch(setLoadingScreen(true));
+        dispatch(setPastAppStates([...past, current]));
+        theme = new Theme(future[0]);
+        theme.validateFonts().then(() => {
+          theme.commit().then(() => dispatch(setLoadingScreen(false)));
+        });
+        dispatch(setFutureAppStates(future.slice(1)));
+        break;
       case "code":
       case "code-btn":
         dispatch(setThemeCode(true));
@@ -26,6 +54,19 @@ const Main = ({ dispatch, future }) => {
   return (
     <Setting title="Utility">
       <ListItem
+        id="undo"
+        onClick={handleClick}
+        button
+        disabled={past.length === 0}
+      >
+        <ListItemText primary="Undo" />
+        <ListItemSecondaryAction>
+          <IconButton edge="end" id="undo-btn" onClick={handleClick}>
+            <Undo />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+      <ListItem
         id="redo"
         onClick={handleClick}
         button
@@ -33,8 +74,8 @@ const Main = ({ dispatch, future }) => {
       >
         <ListItemText primary="Redo" />
         <ListItemSecondaryAction>
-          <IconButton edge="end" id="code-btn" onClick={handleClick}>
-            <Code />
+          <IconButton edge="end" id="redo-btn" onClick={handleClick}>
+            <Redo />
           </IconButton>
         </ListItemSecondaryAction>
       </ListItem>
@@ -51,6 +92,8 @@ const Main = ({ dispatch, future }) => {
 };
 
 const mapStateToProps = (state) => ({
+  past: state.appState.past,
+  current: state.appState.current,
   future: state.appState.future,
 });
 
