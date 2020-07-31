a226ee407e2ab4a43c1ff3bd40fc75b254c0494f
David Andrews <dandrewsuk82@gmail.com>
2020-07-31 11:23:30 +0100

    Autogit: 31-07-2020 11:23

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
-import { Divider, Typography, Box } from "@material-ui/core";
+import { Button, Container, Divider, Typography, Box } from "@material-ui/core";
 import Text from "./Text";
 import Buttons from "./Buttons";
 
@@ -24,34 +24,34 @@ const Preview = ({ twoFonts, primary, secondary, current }) => {
   return current ? (
     <>
       <Box
+        minHeight="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
         p={current.backgrounds.box !== "transparent" ? 4 : 0}
         bgcolor={current.backgrounds.box}
       >
-        <Typography variant="h1">Welcome to your new theme!</Typography>
-        <Typography variant="subtitle1" paragraph>
-          The {twoFonts ? `header font ` : `font `} is{" "}
-          {twoFonts
-            ? `${current.header.family} and the body font is ${current.body.family}`
-            : current.body.family}
-          . The primary color is {colorNames.primary} and the secondary color is{" "}
-          {colorNames.secondary}.
-        </Typography>
-        <Typography paragraph>
-          Consectetur officia assumenda magni cupiditate perspiciatis voluptatem
-          consequuntur? Eos veniam consequatur rem earum corrupti Omnis veniam
-          iste laboriosam neque quibusdam, dolorum Eum possimus maiores magnam
-          doloribus suscipit. Autem numquam numquam delectus magni quam et
-          Temporibus provident optio vel labore tempora? Excepturi vel quis
-          perferendis eos non. Distinctio reiciendis facilis debitis qui cum
-          Aperiam ipsum nesciunt facere debitis nisi eos Officia doloribus ipsa
-          ea eos quo distinctio ipsum Dolore ex nisi quod eos ratione natus
-          blanditiis Voluptatem reiciendis quam qui expedita tempora ipsam
-          Officia harum quia.
-        </Typography>
+        <Container>
+          <Typography variant="h1">Welcome to your new theme!</Typography>
+          <Typography variant="subtitle1" paragraph>
+            The {twoFonts ? `header font ` : `font `} is{" "}
+            {twoFonts
+              ? `${current.header.family} and the body font is ${current.body.family}`
+              : current.body.family}
+            . The primary color is {colorNames.primary} and the secondary color
+            is {colorNames.secondary}.
+          </Typography>
+          <Box mt={2}>
+            <Button>See more</Button>
+            <Button>No thanks</Button>
+          </Box>
+        </Container>
       </Box>
-      <Text />
-      <Buttons />
-      <Divider />
+      <Container>
+        <Text />
+        <Buttons />
+        <Divider />
+      </Container>
     </>
   ) : null;
 };
