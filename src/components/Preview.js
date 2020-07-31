daf48a0413b13e0b2fb04820d3f539488b076103
David Andrews <dandrewsuk82@gmail.com>
2020-07-31 11:18:22 +0100

    Autogit: 31-07-2020 11:18

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -24,33 +24,30 @@ const Preview = ({ twoFonts, primary, secondary, current }) => {
   return current ? (
     <>
       <Box
-        my={4}
         p={current.backgrounds.box !== "transparent" ? 4 : 0}
         bgcolor={current.backgrounds.box}
       >
-        <>
-          <Typography variant="h1">Welcome to your new theme!</Typography>
-          <Typography variant="subtitle1" paragraph>
-            The {twoFonts ? `header font ` : `font `} is{" "}
-            {twoFonts
-              ? `${current.header.family} and the body font is ${current.body.family}`
-              : current.body.family}
-            . The primary color is {colorNames.primary} and the secondary color
-            is {colorNames.secondary}.
-          </Typography>
-          <Typography paragraph>
-            Consectetur officia assumenda magni cupiditate perspiciatis
-            voluptatem consequuntur? Eos veniam consequatur rem earum corrupti
-            Omnis veniam iste laboriosam neque quibusdam, dolorum Eum possimus
-            maiores magnam doloribus suscipit. Autem numquam numquam delectus
-            magni quam et Temporibus provident optio vel labore tempora?
-            Excepturi vel quis perferendis eos non. Distinctio reiciendis
-            facilis debitis qui cum Aperiam ipsum nesciunt facere debitis nisi
-            eos Officia doloribus ipsa ea eos quo distinctio ipsum Dolore ex
-            nisi quod eos ratione natus blanditiis Voluptatem reiciendis quam
-            qui expedita tempora ipsam Officia harum quia.
-          </Typography>
-        </>
+        <Typography variant="h1">Welcome to your new theme!</Typography>
+        <Typography variant="subtitle1" paragraph>
+          The {twoFonts ? `header font ` : `font `} is{" "}
+          {twoFonts
+            ? `${current.header.family} and the body font is ${current.body.family}`
+            : current.body.family}
+          . The primary color is {colorNames.primary} and the secondary color is{" "}
+          {colorNames.secondary}.
+        </Typography>
+        <Typography paragraph>
+          Consectetur officia assumenda magni cupiditate perspiciatis voluptatem
+          consequuntur? Eos veniam consequatur rem earum corrupti Omnis veniam
+          iste laboriosam neque quibusdam, dolorum Eum possimus maiores magnam
+          doloribus suscipit. Autem numquam numquam delectus magni quam et
+          Temporibus provident optio vel labore tempora? Excepturi vel quis
+          perferendis eos non. Distinctio reiciendis facilis debitis qui cum
+          Aperiam ipsum nesciunt facere debitis nisi eos Officia doloribus ipsa
+          ea eos quo distinctio ipsum Dolore ex nisi quod eos ratione natus
+          blanditiis Voluptatem reiciendis quam qui expedita tempora ipsam
+          Officia harum quia.
+        </Typography>
       </Box>
       <Text />
       <Buttons />
