dc0e8cfa4104c1b3d64741a3cafe057175f65c5b
David Andrews <dandrewsuk82@gmail.com>
2020-08-01 14:42:37 +0100

    Autogit: 01-08-2020 14:42

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import axios from "axios";
 
 const Hero = ({ current }) => {
-  return null;
+  const [colorNames, setColorNames] = useState({ primary: "", secondary: "" });
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.color.pizza/v1/${primary.substr(1)},${secondary.substr(1)}`
+      )
+      .then((res) => {
+        setColorNames({
+          primary: res.data.colors[0].name,
+          secondary: res.data.colors[1].name,
+        });
+      });
+  }, [primary, secondary]);
+  return (
+    <Box
+      minHeight="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="flex-end"
+      pb={10}
+      bgcolor={current.backgrounds.box}
+      style={{
+        backgroundImage: `url(${heroImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Container>
+        <Box
+          bgcolor={current.backgrounds.box}
+          p={current.backgrounds.box !== "transparent" ? 4 : 0}
+        >
+          <Typography variant="h1">Welcome to your new theme!</Typography>
+          <Typography variant="subtitle1" paragraph>
+            The {twoFonts ? `header font ` : `font `} is{" "}
+            {twoFonts
+              ? `${current.header.family} and the body font is ${current.body.family}`
+              : current.body.family}
+            . The primary color is {colorNames.primary} and the secondary color
+            is {colorNames.secondary}.
+          </Typography>
+          <Box mt={2}>
+            <Grid container spacing={2}>
+              <Grid item>
+                <Button variant="contained" color="primary">
+                  See more
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => getImage(name.replace(/ /g, "+"))}
+                >
+                  No thanks
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </Container>
+      </Box>
+    </Box>
+  );
 };
 
 const mapStateToProps = (state) => ({
   current: state.appState.current,
+  twoFonts: state.appState.current.twoFonts,
 });
 
 export default connect(mapStateToProps)(Hero);
