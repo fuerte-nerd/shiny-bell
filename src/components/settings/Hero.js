80ce8f0e09aa46d37407765b4b9887756e04062c
David Andrews <dandrewsuk82@gmail.com>
2020-08-02 12:00:38 +0100

    Autogit: 02-08-2020 12:00

--- src/components/settings/Hero.js
+++ src/components/settings/Hero.js
@@ -9,11 +9,21 @@ import {
   IconButton,
 } from "@material-ui/core";
 
+import { ChevronLeft, ChevronRight } from "@material-ui/icons";
+
 const Hero = ({ dispatch, current, boxPosition }) => {
   return (
     <Setting title="Hero">
       <ListItem>
         <ListItemText primary="Box position" secondary={boxPosition} />
+        <ListItemSecondaryAction>
+          <IconButton>
+            <ChevronLeft />
+          </IconButton>
+          <IconButton edge="end">
+            <ChevronRight />
+          </IconButton>
+        </ListItemSecondaryAction>
       </ListItem>
     </Setting>
   );
