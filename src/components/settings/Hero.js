7fdebba8e7a5761a20b29cd4490e89d9050098af
David Andrews <dandrewsuk82@gmail.com>
2020-08-02 17:43:34 +0100

    Autogit: 02-08-2020 17:43

--- src/components/settings/Hero.js
+++ src/components/settings/Hero.js
@@ -49,14 +49,17 @@ const Hero = ({
         break;
       case "overlay-opacity-dec":
         if (overlayOpacity > 0) {
-          theme.hero.overlayOpacity = overlayOpacity - (0.1).toFixed(1);
+          theme.hero.overlayOpacity = parseFloat(
+            (overlayOpacity - 0.1).toFixed(1)
+          );
           new Theme(theme).commit();
         }
         break;
       case "overlay-opacity-inc":
         if (overlayOpacity < 1) {
-          theme.hero.overlayOpacity =
-            overlayOpacity + parseFloat((0.1).toFixed(1));
+          theme.hero.overlayOpacity = parseFloat(
+            (overlayOpacity + 0.1).toFixed(1)
+          );
           new Theme(theme).commit();
         }
         break;
