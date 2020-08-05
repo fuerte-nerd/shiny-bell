dce560b163b56f372162e5777749f1c50f2df990
David Andrews <dandrewsuk82@gmail.com>
2020-08-05 15:07:17 +0100

    Autogit: 05-08-2020 15:07

--- src/components/Anchor.js
+++ src/components/Anchor.js
@@ -1,17 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { Box, useTheme, useMediaQuery, withStyles } from "@material-ui/core";
-
-const styles = {
-  root: {
-    top: -48,
-    position: "relative",
-    "@media (min-width:0px) and (orientation: landscape)": { top: -48 },
-    "@media (min-width:600px)": { top: -48 },
-  },
-};
+import React from "react";
 
 const Anchor = (props) => {
-  return <div id={props.id} className={props.classes.root} />;
+  return <div id={props.id} style={{ top: -48 }} />;
 };
 
-export default withStyles(styles)(Anchor);
+export default Anchor;
