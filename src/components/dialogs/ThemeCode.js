39b5113ef27724c21bf8e4b2819fc99a89955a36
David Andrews <dandrewsuk82@gmail.com>
2020-08-07 18:12:51 +0100

    Autogit: 07-08-2020 18:12

--- src/components/dialogs/ThemeCode.js
+++ src/components/dialogs/ThemeCode.js
@@ -17,21 +17,13 @@ import { Alert } from "@material-ui/lab";
 import { FileCopy } from "@material-ui/icons";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
-import getThemeCode from "../../scripts/getThemeCode";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import AppTypography from "../AppTypography";
 
 import CodeGenerator from "../CodeGenerator";
 
-const ThemeCode = ({
-  dispatch,
-  themeCode,
-  current,
-  font,
-  twoFonts,
-  headerFont,
-}) => {
+const ThemeCode = ({ dispatch, themeCode, current }) => {
   const [codeSnippets, setCodeSnippets] = useState({
     hero: ``,
     theme: ``,
@@ -56,34 +48,8 @@ const ThemeCode = ({
     dispatch(setThemeCode(false));
   };
 
-  const helmetCodeString = () => {
-    return twoFonts && font && headerFont
-      ? `//react-helmet (to fetch Google Fonts)
-<Helmet>
-  <link
-    href={\`https://fonts.googleapis.com/css2?family=${font.family.replace(
-      / /g,
-      "+"
-    )}&family=${headerFont.family.replace(/ /g, "+")}&display=swap\`}
-    rel="stylesheet"
-  />
-</Helmet>
-`
-      : `//react-helmet (to fetch Google Font)
-<Helmet>
-  <link
-    href={\`https://fonts.googleapis.com/css2?family=${font.family.replace(
-      / /g,
-      "+"
-    )}&display=swap\`}
-    rel="stylesheet"
-  />
-</Helmet>
-`;
-  };
-
   return (
-    <Dialog maxWidth="sm" fullWidth open={true} onClose={handleClose}>
+    <Dialog maxWidth="sm" fullWidth open={themeCode} onClose={handleClose}>
       <DialogTitle disableTypography>
         <AppTypography variant="h5">Theme Code</AppTypography>
       </DialogTitle>
@@ -112,7 +78,7 @@ const ThemeCode = ({
           <SyntaxHighlighter
             language="javascript"
             style={tomorrow}
-            customStyle={{ height: "20rem" }}
+            customStyle={{ maxHeight: "20rem" }}
           >
             {current ? codeSnippets.theme : ``}
           </SyntaxHighlighter>
@@ -138,7 +104,7 @@ const ThemeCode = ({
           <SyntaxHighlighter
             language="javascript"
             style={tomorrow}
-            customStyle={{ height: "20rem" }}
+            customStyle={{ maxHeight: "20rem" }}
           >
             {current ? codeSnippets.helmet : ``}
           </SyntaxHighlighter>
@@ -164,7 +130,7 @@ const ThemeCode = ({
           <SyntaxHighlighter
             language="jsx"
             style={tomorrow}
-            customStyle={{ height: "20rem" }}
+            customStyle={{ maxHeight: "20rem" }}
           >
             {current ? codeSnippets.hero : ``}
           </SyntaxHighlighter>
@@ -190,9 +156,6 @@ const ThemeCode = ({
 
 const mapStateToProps = (state) => ({
   themeCode: state.display.themeCode,
-  font: state.appState.current.body,
-  twoFonts: state.appState.current.twoFonts,
-  headerFont: state.appState.current.header,
   current: state.appState.current,
 });
 
