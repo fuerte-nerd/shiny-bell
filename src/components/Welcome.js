4fbf95eab321711479f343c691d62aa2f2d618bf
David Andrews <dandrewsuk82@gmail.com>
2020-08-08 16:59:18 +0100

    Autogit: 08-08-2020 16:59

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,24 +6,28 @@ import {
   DialogContent,
   DialogActions,
   Button,
+  Divider,
 } from "@material-ui/core";
 
 import { GitHub } from "@material-ui/icons";
 
 const Welcome = () => {
   return (
-    <Dialog open={true} fullScreen>
+    <Dialog open={true}>
       <DialogTitle disableTypography>
         <Typography variant="h5" style={{ fontFamily: "Roboto" }}>
           Welcome to MUITA
         </Typography>
       </DialogTitle>
       <DialogContent>
-        <Typography variant="h4">What is MUITA?</Typography>
-        <Typography>
+        <Typography variant="h4" style={{ fontFamily: "Roboto" }}>
+          What is MUITA?
+        </Typography>
+        <Typography style={{ fontFamily: "Roboto" }} paragraph>
           MUITA (Material UI Theme Assistant) is a tool to help you quickly and
           intuitively find a Material UI theme for your next poject!
         </Typography>
+        <Divider />
       </DialogContent>
     </Dialog>
   );
