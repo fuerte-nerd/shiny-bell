551472df82bcc13b858e85bf56b5f112e3afb9a6
David Andrews <dandrewsuk82@gmail.com>
2020-08-08 18:14:58 +0100

    Autogit: 08-08-2020 18:14

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -27,20 +27,22 @@ const Welcome = () => {
         </Typography>
         <Typography style={{ fontFamily: "Roboto" }} paragraph>
           MUITA (Material UI Theme Assistant) is a tool to help you quickly and
-          intuitively find a Material UI theme for your next poject!
+          intuitively find a Material UI theme for your next React project!
         </Typography>
         <Divide />
         <Typography variant="h4" style={{ fontFamily: "Roboto" }}>
           "How does it work?.."
         </Typography>
         <Typography style={{ fontFamily: "Roboto" }} paragraph>
-          It's very simple! You just hit the refresh button and MUITA will
-          provide you with a new theme, including fonts (provided by Google
-          Fonts), colors and an image. If you like certain elements of it, you
-          can lock them in the settings menu and they will remain upon
-          subsequent refreshes. You can narrow the random search parameters via
-          the settings menu. The images are provided by the Unsplash API and
-          even though they are royalty-free, they are meant as more of a guide.
+          It's very simple! You just hit the refresh button (bottom right of
+          your screen) and MUITA will provide you with a new theme at random,
+          including fonts (provided by Google Fonts), colors and an image. If
+          you like certain elements of it, you can lock them in the settings
+          menu and they will remain upon subsequent refreshes. You can narrow
+          the random search parameters via the settings menu. When you are happy
+          with everything, you can copy the code and paste it directly into your
+          project. The images are provided by the Unsplash API and even though
+          they are royalty-free, they are meant as more of a guide.
         </Typography>
         <Divide />
         <Typography variant="h4" style={{ fontFamily: "Roboto" }}>
@@ -62,20 +64,28 @@ const Welcome = () => {
         </Typography>
         <Divide />
         <Typography variant="h4" style={{ fontFamily: "Roboto" }}>
-          "I would like to make an inquiry ..."
+          "I would like to contact you..."
         </Typography>
         <Typography style={{ fontFamily: "Roboto" }} paragraph>
           If you would like to report a bug, donate to the project, help develop
           MUITA, hire me, or get in touch with me for any other reason, my email
           address is{" "}
-          <Link href="mailto:fuertenerd@gmail.com.">fuertenerd@gmail.com</Link>
+          <Link href="mailto:fuertenerd@gmail.com.">fuertenerd@gmail.com</Link>.
         </Typography>
       </DialogContent>
       <DialogActions>
-        <Button style={{ fontFamily: "roboto" }} endIcon={<GitHub />}>
+        <Button
+          id="github"
+          style={{ fontFamily: "roboto" }}
+          endIcon={<GitHub />}
+        >
           View on GitHub
         </Button>
-        <Button style={{ fontFamily: "roboto" }} endIcon={<PlayArrow />}>
+        <Button
+          id="play"
+          style={{ fontFamily: "roboto" }}
+          endIcon={<PlayArrow />}
+        >
           Start playing
         </Button>
       </DialogActions>
