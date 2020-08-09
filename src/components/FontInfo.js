ea60ed85abe52d5b554eb126376db2b080827662
David Andrews <dandrewsuk82@gmail.com>
2020-08-09 14:35:58 +0100

    Autogit: 09-08-2020 14:35

--- src/components/FontInfo.js
+++ src/components/FontInfo.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
+import { Popover } from "@material-ui/core";
 
-const FontInfo = () => {
-  return null;
+const FontInfo = ({ isOpen, body, header }) => {
+  const [mouseLocation, setMouseLocation] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    if (isOpen) {
+      setMouseLocation;
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    document.addEventListener("mouseover", (e) => {
+      setMouseLocation({ x: e.clientX, y: e.clientY });
+    });
+  }, []);
+  return (
+    <Popover
+      anchorReference="anchorPosition"
+      anchorPosition={{ top: mouseLocation.y, left: mouseLocation.x }}
+    >
+      Hello
+    </Popover>
+  );
 };
 
 const mapStateToProps = (state) => ({
