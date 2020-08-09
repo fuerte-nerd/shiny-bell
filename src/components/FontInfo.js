3c845e7a37098e2014b7d93262bdc87353a35676
David Andrews <dandrewsuk82@gmail.com>
2020-08-09 15:06:33 +0100

    Autogit: 09-08-2020 15:06

--- src/components/FontInfo.js
+++ src/components/FontInfo.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { Popover } from "@material-ui/core";
 import { setFontInfo } from "../state/display/actions";
 
-const FontInfo = ({ dispatch, isOpen, body, header }) => {
+const FontInfo = ({ dispatch, isOpen, section, body, header }) => {
   const [mouseLocation, setMouseLocation] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
@@ -11,6 +11,11 @@ const FontInfo = ({ dispatch, isOpen, body, header }) => {
       setMouseLocation({ x: e.clientX, y: e.clientY });
     });
   }, []);
+
+  useEffect(() => {
+    console.log(isOpen);
+    console.log(section);
+  }, [isOpen]);
 
   return (
     <Popover
@@ -25,6 +30,7 @@ const FontInfo = ({ dispatch, isOpen, body, header }) => {
 
 const mapStateToProps = (state) => ({
   isOpen: state.display.fontInfo.isOpen,
+  section: state.display.fontInfo.section,
   body: state.appState.current.body.family,
   header: state.appState.current.header.family,
 });
