3055bce81d3f13dd190b3fb9df74accadca00e0f
David Andrews <dandrewsuk82@gmail.com>
2020-08-09 15:16:47 +0100

    Autogit: 09-08-2020 15:16

--- src/components/FontInfo.js
+++ src/components/FontInfo.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
-import { Popover } from "@material-ui/core";
-import { setFontInfo } from "../state/display/actions";
+import { Popover, Box, Typography } from "@material-ui/core";
 
 const FontInfo = ({ dispatch, isOpen, section, body, header }) => {
   const [mouseLocation, setMouseLocation] = useState({ x: 0, y: 0 });
@@ -12,11 +11,6 @@ const FontInfo = ({ dispatch, isOpen, section, body, header }) => {
     });
   }, []);
 
-  useEffect(() => {
-    console.log(isOpen);
-    console.log(section);
-  }, [isOpen]);
-
   return (
     <Popover
       open={isOpen}
@@ -24,7 +18,11 @@ const FontInfo = ({ dispatch, isOpen, section, body, header }) => {
       anchorPosition={{ top: mouseLocation.y, left: mouseLocation.x }}
       style={{ pointerEvents: "none" }}
     >
-      Hello
+      <Box p={2}>
+        <Typography style={{ fontFamily: "Roboto" }}>
+          {section === "body" ? body : header}
+        </Typography>
+      </Box>
     </Popover>
   );
 };
